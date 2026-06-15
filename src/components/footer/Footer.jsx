f7e9b8c4c2bf55da520021b8aa6bc00cc56e8e53
Bianca Evila <biancaevila24@.com>
2026-06-15 19:39:36 -0300

    att footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,27 +1,36 @@
-import logoFacebook from "../../assets/footer/logoFacebook.png"
-import logoTwitter from "../../assets/footer/logoTwitter.png"
-import logoYoutube from "../../assets/footer/logoYoutube.png"
-import logoLinkedin from "../../assets/footer/logoLinkedin.png"
-import logoInstagram from "../../assets/footer/logoInstagram.png"
-import s from "./footer.module.scss"
-
+import logoFacebook from "../../assets/footer/logoFacebook.png";
+import logoTwitter from "../../assets/footer/logoTwitter.png";
+import logoYoutube from "../../assets/footer/logoYoutube.png";
+import logoLinkedin from "../../assets/footer/logoLinkedin.png";
+import logoInstagram from "../../assets/footer/logoInstagram.png";
+import s from "./footer.module.scss";
 
 export default function Footer() {
-    return(
-        <footer>
-            <section className={s.contatosFooter}>
-                <p>0000-0000</p>
-                <nav>
-                    <a href=""><img src={logoFacebook} alt="Imagem da logo do facebook" /></a>
-                    <a href=""><img src={logoTwitter} alt="Imagem da logo do twitter" /></a>
-                    <a href=""><img src={logoYoutube} alt="Imagem da logo do youtube" /></a>
-                    <a href=""><img src={logoLinkedin} alt="Imagem da logo do linkedin" /></a>
-                    <a href=""><img src={logoInstagram} alt="Imagem da logo do instagram" /></a>
-                </nav>
-            </section>
-            <section className={s.copyRight}>
-                <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024</p>
-            </section>
-        </footer>
-    )
+  return (
+    <footer>
+      <section className={s.contatosFooter}>
+        <p>0000-0000</p>
+        <nav>
+          <a href="">
+            <img src={logoFacebook} alt="Imagem da logo do facebook" />
+          </a>
+          <a href="">
+            <img src={logoTwitter} alt="Imagem da logo do twitter" />
+          </a>
+          <a href="">
+            <img src={logoYoutube} alt="Imagem da logo do youtube" />
+          </a>
+          <a href="">
+            <img src={logoLinkedin} alt="Imagem da logo do linkedin" />
+          </a>
+          <a href="">
+            <img src={logoInstagram} alt="Imagem da logo do instagram" />
+          </a>
+        </nav>
+      </section>
+      <section className={s.copyRight}>
+        <p>Desenvolvido com &hearts; por Bianca - {new Date().getFullYear()}</p>
+      </section>
+    </footer>
+  );
 }
